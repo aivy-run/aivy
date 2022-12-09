71bf58e5407cfa793a9a988ca5d5979deaf5b59e
ddPn08 <pyn.g.1125@gmail.com>
2022-12-09 16:41:01 +0900

    fix following user's page

--- src/routes/images/following.tsx
+++ src/routes/images/following.tsx
@@ -1,7 +1,8 @@
-import { createEffect, createSignal } from 'solid-js'
+import { createEffect, createSignal, Show } from 'solid-js'
 
 import { PostContainer } from '~/components/gallery/post-container'
 import { Posts } from '~/components/gallery/posts'
+import { Fallback } from '~/components/ui/fallback'
 import { useUser } from '~/context/user'
 import { api } from '~/lib/api/supabase'
 
@@ -9,25 +10,38 @@ export default function Latest() {
   const {
     util: { withUser },
   } = useUser(true)
+  const [loading, setLoading] = createSignal(true)
   const [following, setFollowing] = createSignal([] as string[])
 
   createEffect(() => {
     withUser(async ([, profile]) => {
       const relationships = await api.relationship.getRelationships({ authors: [profile.uid] })
       setFollowing(relationships.map((v) => v.target))
+      setLoading(false)
     })
   })
 
   return (
     <PostContainer>
-      <Posts
-        title="フォロー中"
-        all={50}
-        url="/images/following"
-        filter={{
-          authorOr: following(),
-        }}
-      />
+      <Show when={!loading()} fallback={<Fallback height="auto" />}>
+        <Show
+          when={following().length}
+          fallback={
+            <>
+              <h1>誰もフォローしていません</h1>
+            </>
+          }
+        >
+          <Posts
+            title="フォロー中"
+            all={50}
+            url="/images/following"
+            filter={{
+              authorOr: following(),
+            }}
+          />
+        </Show>
+      </Show>
     </PostContainer>
   )
 }
